4c8b86787b92a3932a1aa542048a08ccc8c36352
takuan <ariteku@gmail.com>
2014-10-05 21:15:37 +0900

    タブをスペースに

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-
+# カレントディレクトリにある.ファイルのシンボリックリンクをhomeにはる
 for dotfile in .[!.]?*
 do
-	# .gitはスルー
-	if [ $dotfile = '.git' ]
-	then
-		continue
-	fi
-	
-	ln -Fis "$PWD/$dotfile" $HOME
-
+  # .gitはスルー
+  if [ $dotfile = '.git' ]
+  then
+    continue
+  fi
+  ln -Fis "$PWD/$dotfile" $HOME
 done
